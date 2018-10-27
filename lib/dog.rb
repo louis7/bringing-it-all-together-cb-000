b46f7113927812d367f99c972f0ba23aab6ce26d
louis baptiste <louisvis@ymail.com>
2018-10-27 08:01:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -82,6 +82,7 @@ attr_accessor :name, :breed, :id
         self.new_from_db(row)
       end.first
     end
+    
     def update
    sql = "UPDATE dogs SET name = ?, breed = ?  WHERE id = ?"
    DB[:conn].execute(sql, self.name, self.breed, self.id)
